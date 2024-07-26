7efaf73aba34fbd67a9196ba227a005159e829e3
nguyenquoctien15 <nguyenquoctien08797@gmail.com>
2024-07-26 19:10:48 +0700

    admin

---
+++ src/components/SliderComponent.tsx
@@ -0,0 +1,11 @@
+import { Layout } from 'antd'
+import React from 'react'
+
+const {Slider} = Layout;
+const SliderComponent = () => {
+  return (
+    <Slider collapsed theme> </Slider>
+  )
+}
+
+export default SliderComponent
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
+import { Layout, Slider } from "antd";
 import type { AppProps } from "next/app";
 
+const {Header, Sider, Content} = Layout;
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return 
+  <Layout>
+    <Header/>
+   <Layout>
+   <Slider />
+    <Content>
+      <div className="container-fuild bg-white"></div>
+      <div className="container p-4"></div>
+    <Component {...pageProps} />
+    </Content>
+   </Layout>
+  </Layout>
+  ;
 }
